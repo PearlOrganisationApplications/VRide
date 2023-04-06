0722812741fe4158152b9406898317db78c54d62
NeerajBadiyari <neerajbadiyari@pearlorganisation.com>
2023-04-06 15:40:12 +0530

    changes

--- app/src/main/java/com/pearl/v_ride/DocumentActivity.kt
+++ app/src/main/java/com/pearl/v_ride/DocumentActivity.kt
@@ -1,24 +1,42 @@
 package com.pearl.v_ride
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pearl.test5.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DocumentActivity : AppCompatActivity() {
 
+    lateinit var pan_dob: EditText
     lateinit var ivback: AppCompatImageView
     lateinit var apptitle: AppCompatTextView
     lateinit var updateBT: Button
+    private val myCalendar = Calendar.getInstance()
+    lateinit var adhadharF: ImageView
+    lateinit var adharFrontIV: ImageView
+    lateinit var adhadharRear: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document)
 
+        adhadharF = findViewById(R.id.addfront)
+        adharFrontIV = findViewById(R.id.adharFrotIV)
+        adhadharRear = findViewById(R.id.adharrearIV)
+
+        pan_dob = findViewById(R.id.pan_dobET)
+        pan_dob.setOnClickListener {
+            showDatePicker()
+        }
         updateBT = findViewById<Button>(R.id.updateBT)
 
         ivback=findViewById(R.id.ivBack)
@@ -28,5 +46,48 @@ class DocumentActivity : AppCompatActivity() {
         ivback.setOnClickListener {
             onBackPressed()
         }
+
+
+        // camera
+     /*   adhadharF.setOnClickListener {
+            ImagePicker.with(this)
+//                .galleryOnly()
+                .start()
+        }*/
+    }
+    private fun showDatePicker() {
+        val date = DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, day)
+            updateLabel()
+        }
+
+        val datePickerDialog = DatePickerDialog(
+            this@DocumentActivity,
+            R.style.MyDatePickerDialogTheme, // use your custom theme here
+            date,
+            myCalendar.get(Calendar.YEAR),
+            myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        // Set the maximum and minimum date for the DatePickerDialog
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+        datePickerDialog.datePicker.minDate = getMinimumDate()
+
+        datePickerDialog.show()
+    }
+
+    private fun updateLabel() {
+        val myFormat = "MM/dd/yyyy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        pan_dob.setText(dateFormat.format(myCalendar.time))
+    }
+
+    private fun getMinimumDate(): Long {
+        val minDateCalendar = Calendar.getInstance()
+        minDateCalendar.add(Calendar.YEAR, -100) // Set 100 years ago from now
+        return minDateCalendar.timeInMillis
     }
 }
